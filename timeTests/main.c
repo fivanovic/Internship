4dcb7754f166b3a4fca034f01d20433e65aac306
fivanovic <fivanovic1999@gmail.com>
2022-11-21 05:18:12 -0700

    back to normal?

--- timeTests/main.c
+++ timeTests/main.c
@@ -49,20 +49,20 @@ int main ()
     int fd = open("/sys/bus/pci/devices/0000:01:00.0/resource0", O_RDWR | O_SYNC);
     void* base_address = (void*)Memloc;
     size_t size = sizeval; // 1MiB
-    uint16_t* void_memory = mmap(base_address,
+    void* void_memory = mmap(base_address,
                          size,
                          PROT_READ | PROT_WRITE,
                          MAP_SHARED,
                          fd,
                          0);
-    //uint16_t* memory = (uint16_t*)void_memory;
+    uint16_t* memory = (uint16_t*)void_memory;
 
     //snapshot here
     struct timespec start,end;
 
     clock_gettime(CLOCK_MONOTONIC,&start);
     // Read the value of the first register
-    uint16_t first_register = void_memory[1];
+    uint16_t first_register = memory[1];
 
     //snapshot here
     clock_gettime(CLOCK_MONOTONIC,&end);
@@ -77,14 +77,14 @@ int main ()
     //another snapshot here with a Write operation
 
     clock_gettime(CLOCK_MONOTONIC,&start);
-    void_memory[1] = first_register + 1;
+    memory[1] = first_register + 1;
     clock_gettime(CLOCK_MONOTONIC,&end);
 
     timeSec = (end.tv_sec - start.tv_sec); //+ (double)(end.tv_nsec - start.tv_nsec)/(double)BILLION;
     timeNanoSec = (end.tv_nsec - start.tv_nsec);
     //printf( "Seconds for Write : %lf\n", timeSec );
     printf( "Nanoseconds for Write : %lf\n", timeNanoSec );
-    uint16_t first_register_update = void_memory[1];
+    uint16_t first_register_update = memory[1];
     printf("first register contains %d \n", first_register_update);
 
     return(0);
