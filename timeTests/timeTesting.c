3ed9635f97ce3e5257e0f05dd59f5a5b2a93e0fe
fivanovic <fivanovic1999@gmail.com>
2022-11-21 14:59:03 +0100

    Revert "last hurrah for mutes and spins"
    This reverts commit 3a49b1b43466e893f71ca3af2195203950e6007f.

--- timeTests/timeTesting.c
+++ timeTests/timeTesting.c
@@ -64,8 +64,6 @@ int main ()
     double readTimes[repeats];
     //double wTimes[repeats];
 
-    pthread_spin_lock(&splock);
-    pthread_mutex_lock(&mutlock);
     for (i=0; i<repeats; i++)
     {
         struct timespec start,end;
@@ -144,8 +142,7 @@ int main ()
         //nanosleep(&rqtp,&rmtp);
 
     }
-    pthread_mutex_unlock(&mutlock);
-    pthread_spin_unlock(&splock);
+
     //write anomaly investigation program
     double avgRead = readTimesTotal/(i+1);
     double avgWrite = writeTimesTotal/(i+1);
