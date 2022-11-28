6b01464f2024ec8938dba857f077e267bbb168de
fivanovic <fivanovic1999@gmail.com>
2022-11-28 05:14:29 -0700

    build with 5 mil

--- timeTests/anomalyCollection.c
+++ timeTests/anomalyCollection.c
@@ -50,7 +50,7 @@ int main ()
     minWrite = 10000.0;
     maxWrite = 0.0;
 
-    int repeats = 1000000000;
+    int repeats = 5000000;
 
     double readTimes[repeats];
     //double wTimes[repeats];
